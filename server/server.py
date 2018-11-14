f2a1569187c06b63248ab1f933454bcf5995dd17
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2018-11-14 17:29:33 -0600

    refactor

--- server/server.py
+++ server/server.py
@@ -4,20 +4,23 @@ sys.path.insert(0, '../lib')
 from sender import *
 from pic import *
 
+byte_step = 2048
+
 
 def get_bytes_from_file(filename):
-    return open(filename, "rb").read()
+    f = open(filename, "rb")
+    data = f.read()
+    f.close()
+    return data
 
 
-def onlyUsingUDP(fileName):
+def onlyUsingUDP(vdata):
     s_udp = create_udp_socket()
-    u_bytes = bytearray()
     u_count = 0
-    fileArray = get_bytes_from_file(fileName)
-    for bt in fileArray:
+    for bt in vdata:
         u_bytes.append(bt)
         u_count += 1
-        if u_count == 2048:
+        if u_count == BYTE_STEP:
             send_udp_socket(s_udp, u_bytes)
             u_count = 0
             u_bytes = bytearray()
@@ -25,40 +28,30 @@ def onlyUsingUDP(fileName):
     s_udp.close()
 
 
-def onlyUsingTCP(fileName):
-    t_bytes = bytearray()
-    t_count = 0
-    fileArray = get_bytes_from_file(fileName)
-    for bt in fileArray:
-        t_bytes.append(bt)
-        t_count += 1
-        if t_count == 2048:
-            s_tcp = create_tcp_socket()
-            send_tcp_socket(s_tcp, t_bytes)
-            s_tcp.shutdown(SHUT_WR)
-            s_tcp.close()
-            t_count = 0
-            t_bytes = bytearray()
+def onlyUsingTCP(vdata):
     s_tcp = create_tcp_socket()
-    send_tcp_socket(s_tcp, t_bytes)
+    for i in range(len(vdata), step=BYTE_STEP):
+        if i + BYTE_STEP > len(vdata):
+            send_tcp_socket(s_tcp, vdata[i:])
+        else:
+            send_tcp_socket(s_tcp, vdata[i:i + BYTE_STEP])
     s_tcp.close()
 
 
 ## check input file byte by byte if it is in the given range
-def buffer_frame(fileName, videoRange):
+def buffer_frame(vdata, videoRange):
     s_udp = create_udp_socket()
     t_count = 0
     u_count = 0
     t_bytes = bytearray()
     u_bytes = bytearray()
     prevRange = 0
-    fileArray = get_bytes_from_file(fileName)
-    fileLen = len(fileArray)
+    fileLen = len(vdata)
     for currentRange in videoRange:
         for count in range(prevRange, currentRange[0]):
-            u_bytes.append(fileArray[count])
+            u_bytes.append(vdata[count])
             u_count += 1
-            if u_count == 2048:
+            if u_count == BYTE_STEP:
                 send_udp_socket(s_udp, u_bytes)
                 u_count = 0
                 u_bytes = bytearray()
@@ -66,19 +59,19 @@ def buffer_frame(fileName, videoRange):
         u_count = 0
         u_bytes = bytearray()
         for count in range(currentRange[0], currentRange[1]):
-            t_bytes.append(fileArray[count])
+            t_bytes.append(vdata[count])
             t_count += 1
             count += 1
-            if t_count == 2048:
+            if t_count == BYTE_STEP:
                 tcp_sender(t_bytes)
                 t_count = 0
                 t_bytes = bytearray()
         tcp_sender(t_bytes)
         prevRange = currentRange[1]
     for count in range(prevRange, fileLen):
-        u_bytes.append(fileArray[count])
+        u_bytes.append(vdata[count])
         u_count += 1
-        if u_count == 2048:
+        if u_count == BYTE_STEP:
             send_udp_socket(s_udp, u_bytes)
             u_count = 0
             u_bytes = bytearray()
@@ -100,25 +93,27 @@ if __name__ == '__main__':
     #fileName = '../input.mp4'
     #metaName = '../frame.txt'
 
+    vdata = get_bytes_from_file(fileName)
+
     fs = getFrames(metaName)
     IRange = getIRange(fs)
     PRange = getPRange(fs)
     print(IRange)
 
     tstart = datetime.now()
-    buffer_frame(fileName, IRange)
+    #  buffer_frame(vdata, IRange)
     tend = datetime.now()
     print('TUP(our method) time used:')
     print(tend - tstart)
 
     tstart = datetime.now()
-    onlyUsingTCP(fileName)
+    onlyUsingTCP(vdata)
     tend = datetime.now()
     print('TCP time used:')
     print(tend - tstart)
 
     tstart = datetime.now()
-    onlyUsingUDP(fileName)
+    onlyUsingUDP(vdata)
     tend = datetime.now()
     print('UDP time used:')
     print(tend - tstart)
