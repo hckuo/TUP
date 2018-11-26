f10999db0372fa59d5ee2e2fbf07aeff7b38051e
Hsuan-Chi Kuo <hckuo2@illinois.edu>
2018-11-25 18:13:55 -0600

    remove lib

--- server/server.py
+++ server/server.py
@@ -6,6 +6,32 @@ from pic import *
 
 byte_step = 2048
 
+def create_tcp_socket():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('localhost', 6677))
+    s.listen(5)
+    return s
+
+
+def create_udp_socket():
+    s = socket(AF_INET, SOCK_DGRAM)
+    return s
+
+
+def send_with_connection(conn, data, step=2048):
+    for i in range(0, len(data), step):
+        if i + step > len(data):
+            conn.send(data[i:])
+        else:
+            conn.send(data[i:i+step])
+    conn.close()
+
+def sendto_with_socket(s, data, addr=('localhost', 8888), step=2048):
+    for i in range(0, len(data), step):
+        if i + step > len(data):
+            s.sendto(data[i:], addr)
+        else:
+            s.sendto(data[i:i+step], addr)
 
 def get_bytes_from_file(filename):
     f = open(filename, "rb")
